538db1261b6773e607fe79b4498096e25338e8b1
ammarabrahani <ammarabrahani@gmail.com>
2022-11-26 14:49:49 +0400

    install react router and create links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-import { useEffect, useState } from "react";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Home from "./pages/Home";
 
-const App = () => <div>Ecommerce App</div>;
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
+  );
+};
 
 export default App;
